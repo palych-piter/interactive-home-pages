2be84298ec362e54db8f8e07ef7c5a45b67a2f72
palych-piter <andr.bespalov@gmail.com>
2015-10-18 00:26:39 +0300

    Adding Hibernate Test

---
+++ src/test/java/hibernate/HibernateForApp.java
@@ -0,0 +1,17 @@
+package hibernate;
+
+import org.junit.Test;
+
+/**
+ * Created by Andrey on 10/18/2015.
+ */
+public class HibernateForApp {
+
+    @Test
+    public void UserHibernate() {
+
+
+    }
+
+
+}
