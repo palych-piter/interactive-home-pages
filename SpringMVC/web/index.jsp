ef219fa88fa362c27a078c0a0cf000439f18e056
palych-piter <andr.bespalov@gmail.com>
2015-10-26 00:39:33 +0300

    Changing the dao package class to insert records in User and Message tables, and adding a new SpringMVC module

---
+++ SpringMVC/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Andrey
+  Date: 10/26/2015
+  Time: 12:22 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Interact Home Pages</title>
+</head>
+<body>
+<h1>This is the home page of the HomeBoard!!!!</h1>
+</body>
+</html>
--- src/test/java/dao/JDBCConnect.java
+++ src/test/java/dao/JDBCConnect.java
@@ -13,24 +13,64 @@ public class JDBCConnect {
     public void testConnection() throws SQLException {
 
         //initialize driver
-        //Driver driver = new org.postgresql.Driver();
-        //establish connection
-        Connection conn =
-                DriverManager.getConnection
-                        ("jdbc:postgresql://localhost:5432/HomeBoardDB", "postgres", "Cure1995");
+        Driver driver = new org.postgresql.Driver();
 
-        //select records
+        //establish connection
+        Connection conn = DriverManager.getConnection
+                ("jdbc:postgresql://localhost:5432/HomeBoardDB", "postgres", "Cure1995");
+
         Statement stmt = conn.createStatement();
-        ResultSet resultSet = stmt.executeQuery("SELECT * FROM \"Message\"");
-        //Handle result set
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString("author"));
-        }
 
         //update records
-        stmt.executeUpdate("UPDATE \"Message\" " +
-                "SET Author = 'No Available' WHERE Author = 'Masha'");
+        /* stmt.executeUpdate("UPDATE \"message\" " +
+                "SET Author = 'No Available' WHERE Author = 'Masha'"); */
 
+
+        //DELETE from User and Message Tables
+        stmt.executeUpdate("DELETE FROM \"Message\" WHERE id = 1");
+        stmt.executeUpdate("DELETE FROM \"Message\" WHERE id = 2");
+        stmt.executeUpdate("DELETE FROM \"User\" WHERE id = 1");
+        stmt.executeUpdate("DELETE FROM \"User\" WHERE id = 2");
+
+
+        //INSERT TEST User records
+        stmt.executeUpdate("INSERT INTO \"User\" " +
+                "(id, email, name, status)" +
+                "VALUES (1, 'andr.bespalov@gmail.com', 'Andrey', 'active')");
+
+        stmt.executeUpdate("INSERT INTO \"User\" " +
+                "(id, email, name, status)" +
+                "VALUES (2, 'mbespalova@gmail.com', 'Masha', 'active')");
+
+        //INSERT TEST Message records
+        stmt.executeUpdate
+                ("INSERT INTO \"Message\" " +
+                        "(id, content, picture_link, \"timestamp\", user_pkey) " +
+                        "VALUES (1, 'content of the message2', 'link1', '2015-10-23', 1)");
+
+
+        //INSERT TEST Message records
+        stmt.executeUpdate
+                ("INSERT INTO \"Message\" " +
+                        "(id, content, picture_link, \"timestamp\", user_pkey) " +
+                        "VALUES (2, 'content of the message2', 'link2', '2015-10-23', 2)");
+
+
+        //select records
+        ResultSet resultSet =
+                stmt.executeQuery
+                        (
+                                "SELECT * FROM \"Message\" " +
+                                        "LEFT JOIN \"User\" " +
+                                        "ON (\"User\".id = \"Message\".user_pkey)"
+                        );
+        //Handle result set
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("name"));
+        }
+
+        //Close a connection
         conn.close();
+
     }
 }
